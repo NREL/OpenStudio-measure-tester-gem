6978ad54efa4da23245af17dcf83a87b6c474f02
Nicholas Long <nicholas.long@nrel.gov>
2021-02-15 10:39:23 -0700

    update copyrights

--- spec/test_measures/RotateBuilding/tests/RotateBuilding_Test.rb
+++ spec/test_measures/RotateBuilding/tests/RotateBuilding_Test.rb
@@ -1,5 +1,5 @@
 ######################################################################
-#  Copyright (c) 2008-2020, Alliance for Sustainable Energy.
+#  Copyright (c) 2008-2021, Alliance for Sustainable Energy.
 #  All rights reserved.
 #
 #  This library is free software you can redistribute it and/or
