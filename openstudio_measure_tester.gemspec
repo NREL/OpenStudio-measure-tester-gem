e73217994e56f27cb045c103d04d6b1dbcb2f547
Nicholas Long <nicholas.long@nrel.gov>
2025-04-04 15:10:06 -0600

    update rubocop with new formats

--- openstudio_measure_tester.gemspec
+++ openstudio_measure_tester.gemspec
@@ -32,11 +32,11 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'git', '~> 1.12.0'
   spec.add_dependency 'minitest', '~> 5.14.0'
   spec.add_dependency 'minitest-reporters', '~> 1.4.2'
-  spec.add_dependency 'rake', '~> 13.0'
   spec.add_dependency 'parser', '3.2.2.2'
+  spec.add_dependency 'rake', '~> 13.0'
   spec.add_development_dependency 'rubocop', '1.50'
-  spec.add_development_dependency 'rubocop-performance', '1.20.0'
   spec.add_development_dependency 'rubocop-checkstyle_formatter', '0.6.0'
+  spec.add_development_dependency 'rubocop-performance', '1.20.0'
   spec.add_dependency 'simplecov', '0.22.0'
 
   spec.add_development_dependency 'bundler', '2.4.10'
--- spec/github_actions_report_spec.rb
+++ spec/github_actions_report_spec.rb
@@ -12,17 +12,17 @@ RSpec.describe OpenStudioMeasureTester::MinitestResult do
     gha_reporter.make_minitest_step_summary_table
 
     expected = [
-       "| Metric           | Value  |",
-       "| ---------------- | ------ |",
-       "| Total Tests      | 16     |",
-       "| Load Error       | 0      |",
-       "| Passed           | 13     |",
-       "| Success Rate     | 86.67% |",
-       "| Failures         | 1      |",
-       "| Errors           | 1      |",
-       "| Skipped          | 1      |",
-       "| Incompatible     | 1      |",
-       "| Total Assertions | 120    |",
+      '| Metric           | Value  |',
+      '| ---------------- | ------ |',
+      '| Total Tests      | 16     |',
+      '| Load Error       | 0      |',
+      '| Passed           | 13     |',
+      '| Success Rate     | 86.67% |',
+      '| Failures         | 1      |',
+      '| Errors           | 1      |',
+      '| Skipped          | 1      |',
+      '| Incompatible     | 1      |',
+      '| Total Assertions | 120    |'
     ]
 
     expect(gha_reporter.minitest_summary_table).to eq expected.join("\n")
